804b0f202c3139a217018ea5cc55d01d67952ee5
E2rPhiphatphong <48234153+E2rPhiphatphong@users.noreply.github.com>
2022-02-15 10:20:15 +0700

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,20 @@
+
+
+var mySlider = new rSlider({
+
+target: '#sampleSlider',
+
+values: [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015],
+
+range: false,
+
+tooltip: false,
+
+scale: true,
+
+labels: true,
+
+set: [2010, 2013]
+
+});
+
